8bbaffeaf04a2a6e74d17e41f98dc0058b12ce39
dn0418 <daniel.noble0418@gmail.com>
2023-06-11 01:37:02 +0600

    create car table

--- src/cars/cars.dto.ts
+++ src/cars/cars.dto.ts
@@ -1,2 +1,54 @@
-export class CreateCarDto { }
+import { IsBoolean, IsNotEmpty, IsOptional, IsString } from "class-validator";
+
+
+export class CreateCarDto {
+  @IsString()
+  @IsNotEmpty()
+  name: string;
+
+  @IsString()
+  @IsNotEmpty()
+  thumbnail: string;
+
+  @IsString()
+  @IsNotEmpty()
+  model: string;
+
+  @IsString()
+  @IsNotEmpty()
+  carNo: string;
+
+  @IsString()
+  @IsNotEmpty()
+  startedDate: string;
+
+  @IsString()
+  @IsNotEmpty()
+  endDate: string;
+
+  @IsString()
+  @IsNotEmpty()
+  seatNo: string;
+
+  @IsBoolean()
+  @IsNotEmpty()
+  isDriver: boolean;
+
+  @IsString()
+  @IsNotEmpty()
+  description: string;
+
+  @IsString()
+  @IsNotEmpty()
+  price: string;
+
+  @IsString()
+  @IsOptional()
+  discountedPrice: string;
+
+  @IsString()
+  @IsOptional()
+  driverId: string;
+}
+
 export class UpdateCarDto { }
